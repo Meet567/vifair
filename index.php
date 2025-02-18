0335aa4bf685298548f7d2764f26a3eccba5e4e2
PatelDhruv11 <dhruv.patel02012003@gmail.com>
2025-02-18 10:32:32 +0530

    all change

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Vibrant India Fair </title>
     <?php
-        include "include/header.php";
+    include "include/header.php";
     ?>
 </head>
+
 <body>
     <!-- Banner Section -->
     <section>
@@ -23,61 +25,61 @@
         </div>
     </section>
     <!-- End Banner Section  -->
-     
+
     <!-- OWL Carousel -->
-     <div class="section-margin">
+    <div class="section-margin">
         <div class="primary-heading"> Upcoming Events </div>
         <div class="owl-carousel-box">
-        <div class="owl-carousel owl-theme">
-            <div class="item event-main-cart">
-                <img src="Assets/Image/Delhi.png" alt="">
-                <div class="event-details-box">
-                    <div class="event-city-date-box">
-                        <div class="event-city">
-                            New Delhi
+            <div class="owl-carousel owl-theme">
+                <div class="item event-main-cart">
+                    <img src="Assets/Image/Delhi.png" alt="">
+                    <div class="event-details-box">
+                        <div class="event-city-date-box">
+                            <div class="event-city">
+                                New Delhi
+                            </div>
+                            <div class="event-date">
+                                Date: 15-02-2025
+                            </div>
                         </div>
-                        <div class="event-date">
-                            Date: 15-02-2025
+                        <div class="event-location">
+                            Yeshobhoomi, Dwarka, Sector-25 New Delhi, India
                         </div>
-                    </div>
-                    <div class="event-location">
-                        Yeshobhoomi, Dwarka, Sector-25 New Delhi, India
                     </div>
                 </div>
-            </div>
-            <div class="item event-main-cart">
-                <img src="Assets/Image/Channai.jpg" alt="">
-                <div class="event-details-box">
-                    <div class="event-city-date-box">
-                        <div class="event-city">
-                            CHANNAI
+                <div class="item event-main-cart">
+                    <img src="Assets/Image/Channai.jpg" alt="">
+                    <div class="event-details-box">
+                        <div class="event-city-date-box">
+                            <div class="event-city">
+                                CHANNAI
+                            </div>
+                            <div class="event-date">
+                                Date: 15-02-2025
+                            </div>
                         </div>
-                        <div class="event-date">
-                            Date: 15-02-2025
+                        <div class="event-location">
+                            Channai Trade Center
                         </div>
-                    </div>
-                    <div class="event-location">
-                        Channai Trade Center
                     </div>
                 </div>
-            </div>
-            <div class="item event-main-cart">
-                <img src="Assets/Image/Mumbai.png" alt="">
-                <div class="event-details-box">
-                    <div class="event-city-date-box">
-                        <div class="event-city">
-                            MUMBAI
+                <div class="item event-main-cart">
+                    <img src="Assets/Image/Mumbai.png" alt="">
+                    <div class="event-details-box">
+                        <div class="event-city-date-box">
+                            <div class="event-city">
+                                MUMBAI
+                            </div>
+                            <div class="event-date">
+                                Date: 15-02-2025
+                            </div>
                         </div>
-                        <div class="event-date">
-                            Date: 15-02-2025
+                        <div class="event-location">
+                            Bombay Exhibition Center
                         </div>
-                    </div>
-                    <div class="event-location">
-                        Bombay Exhibition Center
                     </div>
                 </div>
-            </div>
-            <!-- <div class="item event-main-cart">
+                <!-- <div class="item event-main-cart">
                 <img src="https://acetechexpo.com/wp-content/uploads/2025/01/shutterstock_1747178798-2.png" alt="">
                 <div class="event-details-box">
                     <div class="event-city-date-box">
@@ -93,7 +95,7 @@
                     </div>
                 </div>
             </div> -->
-            <!-- <div class="item event-main-cart">
+                <!-- <div class="item event-main-cart">
                 <img src="https://img.freepik.com/free-photo/futuristic-city-architecture-design_1409-5713.jpg?t=st=1739772307~exp=1739775907~hmac=20302289970774fd620aaf7dfb06a85c50c2e6018507a48fe7c37155763e91be&w=826" alt="">
                 <div class="event-details-box">
                     <div class="event-city-date-box">
@@ -109,92 +111,93 @@
                     </div>
                 </div>
             </div> -->
-        </div>
-        <div>
-            <div class="owl-nav-box">
-                <button type="button" class="owl-pre-btn"> Prev </button>
-                <button type="button" class="owl-next-btn"> Next </button>
             </div>
-        </div>
-    </div>
-    <!-- ENd OWl Carousel -->
-
-    <!-- Visit Exhibit Section -->
-     
-    <section class="visit-exhibit-main section-margin">
-        <div class="container">
-            <div class="row d-flex align-items-center g-4">
-                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                    <div class="visit-main-box">
-                        <div class="visit-attachment">
-                            <picture>
-                                <img src="https://acetechexpo.com/wp-content/uploads/2024/03/exhibit-2.jpg" alt="">
-                            </picture>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                    <div class="visit-content-box">
-                        <div class="primary-title">
-                            I Want To Visit
-                        </div>  
-                        <div class="visit-content-first">
-                            Vibrant India Fair 2025-2026: More than an exhibition, it’s an experience. Will you be there?
-                        </div>
-                        <p class="visit-content-second">
-                            How can I get the Pass? <span><a class="visit-link" href="#">I WANT TO VISIT </a> </span>
-                        </p>
-                        <div class="visit-content-third">
-                            Vibrant India Fair: Beyond the Exhibition, Into Architectural Brilliance
-                        </div>
-                        <p class="visit-content-fourth">
-                            Forget the ordinary exhibition experience. Vibrant India Fair is where you’ll witness the evolution of the industry firsthand, immersed in cutting-edge innovation and groundbreaking solutions.
-                        </p>
-                        <div class="primary-btn-box">
-                            <a class="primary-btn" href="#"><span>READ MORE</span></a> 
-                        </div>
-                    </div>
-                </div>
-
-                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                    <div class="visit-content-box">
-                        <div class="primary-title">
-                            I Want To Exhibit
-                        </div>
-                        <p class="visit-content-second">
-                            I want to know about Booth Booking, <span><a class="visit-link" href="#">I WANT TO EXHIBIT </a> </span>
-                        </p>  
-                        <div class="visit-content-first">
-                            Meet new buyers. Grow sales. Sign new deals.
-                        </div>
-                        
-                        <p class="visit-content-fourth">
-                            It’s all possible at VIBRANT INDIA FAIR, The ultimate architecture, building materials & design B2B exhibition. Elevate Your Business: Connect, Grow, Succeed at VIBRANT INDIA FAIR
-                        </p>
-                        <div class="primary-btn-box">
-                            <a class="primary-btn" href="#"><span> READ MORE </span></a> 
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
-                    <div class="visit-main-box">
-                        <div class="visit-attachment">
-                            <picture>
-                                <img src="https://acetechexpo.com/wp-content/uploads/2024/03/visit-2.jpg" alt="">
-                            </picture>
-                        </div>
-                    </div>
+            <div>
+                <div class="owl-nav-box">
+                    <button type="button" class="owl-pre-btn"> Prev </button>
+                    <button type="button" class="owl-next-btn"> Next </button>
                 </div>
             </div>
         </div>
-    </section>
+        <!-- ENd OWl Carousel -->
 
-    
-    <!-- Visit Exhibit Section -->
+        <!-- Visit Exhibit Section -->
 
-    
-    <?php
+        <section class="visit-exhibit-main section-margin">
+            <div class="container">
+                <div class="row d-flex align-items-center g-4">
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                        <div class="visit-main-box">
+                            <div class="visit-attachment">
+                                <picture>
+                                    <img src="https://acetechexpo.com/wp-content/uploads/2024/03/exhibit-2.jpg" alt="">
+                                </picture>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                        <div class="visit-content-box">
+                            <div class="primary-title">
+                                I Want To Visit
+                            </div>
+                            <div class="visit-content-first">
+                                Vibrant India Fair 2025-2026: More than an exhibition, it’s an experience. Will you be there?
+                            </div>
+                            <p class="visit-content-second">
+                                How can I get the Pass? <span><a class="visit-link" href="#">I WANT TO VISIT </a> </span>
+                            </p>
+                            <div class="visit-content-third">
+                                Vibrant India Fair: Beyond the Exhibition, Into Architectural Brilliance
+                            </div>
+                            <p class="visit-content-fourth">
+                                Forget the ordinary exhibition experience. Vibrant India Fair is where you’ll witness the evolution of the industry firsthand, immersed in cutting-edge innovation and groundbreaking solutions.
+                            </p>
+                            <div class="primary-btn-box">
+                                <a class="primary-btn" href="#"><span>READ MORE</span></a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                        <div class="visit-content-box">
+                            <div class="primary-title">
+                                I Want To Exhibit
+                            </div>
+                            <p class="visit-content-second">
+                                I want to know about Booth Booking, <span><a class="visit-link" href="#">I WANT TO EXHIBIT </a> </span>
+                            </p>
+                            <div class="visit-content-first">
+                                Meet new buyers. Grow sales. Sign new deals.
+                            </div>
+
+                            <p class="visit-content-fourth">
+                                It’s all possible at VIBRANT INDIA FAIR, The ultimate architecture, building materials & design B2B exhibition. Elevate Your Business: Connect, Grow, Succeed at VIBRANT INDIA FAIR
+                            </p>
+                            <div class="primary-btn-box">
+                                <a class="primary-btn" href="#"><span> READ MORE </span></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-6 col-lg-6 col-md-6 col-sm-6">
+                        <div class="visit-main-box">
+                            <div class="visit-attachment">
+                                <picture>
+                                    <img src="https://acetechexpo.com/wp-content/uploads/2024/03/visit-2.jpg" alt="">
+                                </picture>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+
+        <!-- Visit Exhibit Section -->
+
+
+        <?php
         include "include/footer.php";
-    ?>
+        ?>
 </body>
+
 </html>
