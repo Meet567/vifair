4d2b90236129ad97a937cb663ae654fabe2dccc8
Meet567 <vasoyameet1999@gmail.com>
2025-02-27 16:34:50 +0530

    mpUpadte

---
+++ exhibitor_profile.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Exhibitor Profile </title>
+    <?php
+        include "include/header.php";
+    ?>
+</head>
+
+    <!-- <div>
+        <img src="https://wordpress.mapsted.com/wp-content/uploads/2023/02/TradeShowBlog.jpg" alt="">
+    </div> -->
+    <section class="secion-margin mt-5">
+        <div class="main main-div">
+            <div class="container-fluid">
+                <div class="primary-heading">Exhibitor Profile</div>
+                <div class="row">
+                    <div class="col-xl-8 col-lg-8 col-md-8 col-sm-12">
+                        <div class="faq-accordion"></div>
+                    </div>
+                    <div class="col-xl-4 col-lg-4 col-md-4 col-sm-12">
+                        <div class="right-part-message-box">
+                            <div class="right-part-sub-box">
+                                <div class="right-part-title">Say Hello!</div>
+                                <p class="right-part-paragraph">Join us in shaping the future of COMPLETE HOUSEWARE EXHIBITION 2025 at our next exhibition and discover the possibilities that await.</p>
+                            </div> 
+                            <div class="right-main-img-box">
+                                <div class="right-img-box">
+                                    <img class="right-part-img" src="https://lizza.wpengine.com/lms/wp-content/uploads/sites/12/2024/02/Rectangle-38.webp" alt="">
+                                    <div class="right-message-link-box">
+                                        <div class="right-message-icon-box">
+                                            <img class="right-message-icon" src="assets/images/icon/chat.png" alt="">
+                                        </div>
+                                        <div class="right-email-box">
+                                            <div class="right-message-heading">VIBRANT INDIA FAIR</div>
+                                           <a class="right-part-email-link" href="#">info@vibrantindiafair.com</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>       
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <?php
+        include "include/footer.php";
+    ?>
+</body>
+
+</html>
